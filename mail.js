45e284f0a55109b4688933b0f48ff6c34cdb61e7
NAOlink <46680768+NAOlink@users.noreply.github.com>
2019-02-07 14:45:36 +0100

    naolink

---
+++ mail.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+	var myform = $("form#myform");
+	myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "naolink@itiscuneo.eu";
+  var template_id = "template_9CKjVwQU";
+
+  myform.find("button").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("Sent!");
+       myform.find("button").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Send");
+    });
+  return false;
+});
+
